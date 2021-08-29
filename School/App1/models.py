686d79cb93204a2f09a75a2877df85cd90e4d128
Py4VB <vatsalbabel06@gmail.com>
2021-08-29 09:43:24 +0530

    Creating a sample model Student

--- School/App1/models.py
+++ School/App1/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+#Creating a db table
+class Student(models.Model):
+    rollno = models.CharField(max_length=3, unique=True)
+    name = models.TextField()
+    
+    def uploaddoc(self, filename):
+        return './documents/' + filename
+
+    documents = models.FileField(upload_to=uploaddoc, null=True, blank=True)
--- School/School/settings.py
+++ School/School/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'App1',
+    'rest-framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
